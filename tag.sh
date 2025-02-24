bf8c0dc862e4d8f42a56432db31d1ef239bf33bb
David Schulz <ds@ironwhale.com>
2025-02-24 12:36:09 +0100

    Changes

--- tag.sh
+++ tag.sh
@@ -7,7 +7,7 @@ git push
 next_version=$(git tag | sort -V | tail -n 1 | sed 's/v//' | awk -F. -v OFS=. '{$NF+=1; print "v"$1,$2,$3}')
 
 echo "Creating new Tag $next_version in 3 seconds"
-sleep 3
+#sleep 3
 
 git tag -a $next_version -m "$next_version"
 git push origin --tags
