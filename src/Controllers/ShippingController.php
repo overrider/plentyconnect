82f5b85c9616eadd4797282dd9a76c6e9f9c08f2
David Schulz <ds@ironwhale.com>
2025-02-26 23:09:01 +0100

    Changes

--- src/Controllers/ShippingController.php
+++ src/Controllers/ShippingController.php
@@ -94,7 +94,7 @@ class ShippingController extends Controller
      */
     private $config;
 
-    private $plugin_revision = 2;
+    private $plugin_revision = 3;
 
 	/**
 	 * ShipmentController constructor.
